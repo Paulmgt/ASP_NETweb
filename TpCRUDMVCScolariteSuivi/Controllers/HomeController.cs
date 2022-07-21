a5d4ad63960337659d2c9ef2819e26c2881705aa
mosab ben <benamaramosab@yahoo.fr>
2022-07-22 00:35:24 +0200

    affichage des modules sur la page d'accueil

--- TpCRUDMVCScolariteSuivi/Controllers/HomeController.cs
+++ TpCRUDMVCScolariteSuivi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using TpCRUDMVCScolariteSuivi.Models;
 
@@ -7,16 +8,21 @@ namespace TpCRUDMVCScolariteSuivi.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ScolariteDbEntities _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ScolariteDbEntities context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return _context.Modules != null ?
+                        View(await _context.Modules.ToListAsync()) :
+                        Problem("Entity set 'ScolariteDbEntities.Modules'  is null.");
         }
+
 
         public IActionResult Privacy()
         {
