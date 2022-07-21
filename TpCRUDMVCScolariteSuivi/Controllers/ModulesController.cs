7146e802cad7929de625e559c6c8a91e73281342
Paulmgt <paul-maingault@outlook.fr>
2022-07-21 16:43:53 +0200

    ajout autorize module

--- TpCRUDMVCScolariteSuivi/Controllers/ModulesController.cs
+++ TpCRUDMVCScolariteSuivi/Controllers/ModulesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using TpCRUDMVCScolariteSuivi.Models;
 
 namespace TpCRUDMVCScolariteSuivi.Controllers
 {
+    [Authorize]
     public class ModulesController : Controller
     {
         private readonly ScolariteDbEntities _context;
---
+++ TpCRUDMVCScolariteSuivi/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace TpCRUDMVCScolariteSuivi.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; } 
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public string Email { get; set; }
+
+        public string Adresse { get; set; }
+
+
+    }
+}
